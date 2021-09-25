795345c82618547e73601d83c4809d0547428419
Otavio Lamartine <otaviollneto@gmail.com>
2021-09-25 00:23:03 -0300

    first

---
+++ build/precache-manifest.0f99bb2ddb44e60c5b78c99f3deaf4ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037aba91799b7ed1c7f3c64582d843e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f54388d5f62fbf77b7e6",
+    "url": "/static/css/2.d6b688d5.chunk.css"
+  },
+  {
+    "revision": "81c6012a33d85e68d4dd",
+    "url": "/static/css/main.7e169c8b.chunk.css"
+  },
+  {
+    "revision": "f54388d5f62fbf77b7e6",
+    "url": "/static/js/2.27441d85.chunk.js"
+  },
+  {
+    "revision": "ed344764e96e72a32d0bd51aea29b8b2",
+    "url": "/static/js/2.27441d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81c6012a33d85e68d4dd",
+    "url": "/static/js/main.1aa60b3f.chunk.js"
+  },
+  {
+    "revision": "5ff6df50c1a535018958",
+    "url": "/static/js/runtime-main.df338948.js"
+  }
+]);
